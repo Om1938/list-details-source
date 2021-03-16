4ce47e585ec012189c1e651939b9fc1c7933d0cc
Om Prakash Das <omkletu@gmail.com>
2021-03-16 17:07:27 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface List {
+  Date?: Date;
+  ListName: string;
+  NoOfEntities: number;
+  description?: string;
+  elements?: string[]
+}
+const ELEMENT_DATA: List[] = [
+  { Date: undefined, ListName: "Competitive Intelligence", NoOfEntities: 0 },
+  { Date: undefined, ListName: "MyVendors", NoOfEntities: 0 },
+  { Date: undefined, ListName: "My Customers", NoOfEntities: 0 },
+  {
+    Date: new Date('07/23/2020'), ListName: "Test_30_results", NoOfEntities: 28, description: "This is Test 20 results",
+    elements: ["Reliance Industries Ltd.", "Indian Oil Corporation Ltd.", "Oil And Natural Gas Corporation Ltd.", "State Bank of Indi", "Bharat Petroleum Corporation Ltd.", "Hindustan Petroleum Corporation Ltd.", "Tata Motors Ltd.", "Rajesh Exports Ltd.", "Tata Consultancy Services Ltd.","Reliance Industries Ltd.", "Indian Oil Corporation Ltd.", "Oil And Natural Gas Corporation Ltd.", "State Bank of Indi", "Bharat Petroleum Corporation Ltd.", "Hindustan Petroleum Corporation Ltd.", "Tata Motors Ltd.", "Rajesh Exports Ltd.", "Tata Consultancy Services Ltd.","Reliance Industries Ltd.", "Indian Oil Corporation Ltd.", "Oil And Natural Gas Corporation Ltd.", "State Bank of Indi", "Bharat Petroleum Corporation Ltd.", "Hindustan Petroleum Corporation Ltd.", "Tata Motors Ltd.", "Rajesh Exports Ltd.", "Tata Consultancy Services Ltd."]
+  },
+  { Date: new Date('06/28/2020'), ListName: "To_index", NoOfEntities: 100 },
+  { Date: new Date('04/10/2020'), ListName: "KMPG Requested Companies-Listed Set_Prasada Kumar", NoOfEntities: 34 },
+  { Date: new Date('03/12/2020'), ListName: "Error Case-LTB To EBITDA Blank_Prasada Kumar", NoOfEntities: 24 },
+  { Date: new Date('12/14/2020'), ListName: "Two Companies", NoOfEntities: 2 },
+  { Date: new Date('11/01/2020'), ListName: "Custom", NoOfEntities: 30 },
+  { Date: new Date('10/29/2020'), ListName: "Mumbai", NoOfEntities: 3 },
+  { Date: new Date('06/23/2020'), ListName: "Delhi", NoOfEntities: 10 },
+  { Date: new Date('01/11/2020'), ListName: "Rajasthan", NoOfEntities: 50 },
+  { Date: undefined, ListName: "Outide Nation", NoOfEntities: 0 },
+  { Date: undefined, ListName: "International", NoOfEntities: 0 },
+]
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'list-details';
+
+  displayedColumns: string[] = ['Date', 'ListName', 'NoOfEntities', 'Actions', 'details'];
+  dataSource = new MatTableDataSource<List>(ELEMENT_DATA);
+  selection = new SelectionModel<List>(false, []);
+  descObject: List
+
+  ngOnInit(): void {
+    this.dataSource.filterPredicate = (data, filter) => {
+      return !filter || data.ListName.toLowerCase().includes(filter)
+    }
+  }
+
+  constructor() {
+    this.descObject = { ListName: "", NoOfEntities: 0, Date: new Date() }
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  showDetails(element: List) {
+    if(this.selection.isSelected(element)){
+      this.descObject = element;
+    }else{
+      this.descObject = { ListName: "", NoOfEntities: 0, Date: new Date() }
+    }
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table'
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatButtonModule} from '@angular/material/button'
+import {MatInputModule} from '@angular/material/input'
+import {MatDividerModule} from '@angular/material/divider'
+import {MatCardModule} from '@angular/material/card'
+import {MatListModule} from '@angular/material/list'
+// import {MatButtonModule} from '@angular/material'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,MatToolbarModule,
+    MatButtonModule,MatInputModule,
+    MatDividerModule,MatCardModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
